8a71b3fc4a7dda024aefd97a99dcad467837a625
Andrey Shmelev <andrew.shmelev@gmail.com>
2014-08-06 14:53:17 +0400

    Добавил модель и сгенерировал контекст и webapi-контроллер

---
+++ HabrData/Models/Post.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace HabrData.Models
+{
+    public class Post
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+
+        public DateTime PublicationDate { get; set; }
+
+        public string Excerpt { get; set; }
+    }
+}
