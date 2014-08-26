7c341c090eaede045103492df5326a8bd71dca01
Andrey Shmelev <andrew.shmelev@gmail.com>
2014-08-27 01:32:41 +0400

    -

--- DataMiner/Parsers/PageParser.cs
+++ DataMiner/Parsers/PageParser.cs
@@ -2,7 +2,6 @@
 using System.Collections.Generic;
 using System.Linq;
 using DataMiner.Domain;
-using DataMiner.Structures;
 using HtmlAgilityPack;
 
 namespace DataMiner.Parsers
--- DataMiner/Parsers/PostParser.cs
+++ DataMiner/Parsers/PostParser.cs
@@ -4,7 +4,6 @@ using System.Linq;
 using System.Text.RegularExpressions;
 using DataMiner.Domain;
 using DataMiner.Helpers;
-using DataMiner.Structures;
 using HtmlAgilityPack;
 
 namespace DataMiner.Parsers
